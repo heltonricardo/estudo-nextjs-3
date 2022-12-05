eac79fd78b34625d3477448e4693c9373c52140f
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2022-12-05 14:35:51 -0300

    configuracao e teste do tailwind para tema dark

--- 07-admin-template/src/components/template/Layout.tsx
+++ 07-admin-template/src/components/template/Layout.tsx
@@ -10,10 +10,15 @@ interface Props {
 
 export default function Layout(props: Props) {
   return (
-    <>
+    <div className="flex h-screen w-screen">
       <MenuLateral />
-      <Titulo {...props} />
-      <Conteudo>{props.children}</Conteudo>
-    </>
+      <div
+        className={`flex flex-col w-full p-7 bg-gray-300
+                dark dark:bg-red-900`}
+      >
+        <Titulo {...props} />
+        <Conteudo>{props.children}</Conteudo>
+      </div>
+    </div>
   );
 }
--- 07-admin-template/tailwind.config.js
+++ 07-admin-template/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
