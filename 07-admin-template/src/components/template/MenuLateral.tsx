e451b2ca974b83896aeef704d945f04584bea6e4
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2022-12-06 10:41:14 -0300

    criando icone da barra lateral

--- 07-admin-template/src/components/template/MenuLateral.tsx
+++ 07-admin-template/src/components/template/MenuLateral.tsx
@@ -1,9 +1,19 @@
 import { IconeCasa, IconeEngrenagem, IconeSino } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
   return (
     <aside>
+      <div
+        className={`
+          h-20 w-20
+          flex flex-col justify-center items-center
+          bg-gradient-to-r from-gray-800 to-gray-700
+        `}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeEngrenagem} />
