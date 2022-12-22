9cdda684456706c5cf8dab80510bc18d70fde71a
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2022-12-22 11:38:30 -0300

    finalizando estilos da tela auth

--- 07-admin-template/src/pages/auth.tsx
+++ 07-admin-template/src/pages/auth.tsx
@@ -16,9 +16,16 @@ export default function Auth() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="w-1/2">
-        <h1 className="text-xl font-bold mb-5">
+    <div className="flex justify-center items-center h-screen">
+      <div className="hidden md:block md:w-1/2 lg:w-2/3">
+        <img
+          className="h-screen w-full object-cover"
+          src="https://source.unsplash.com/random"
+          alt="Imagem da tela de autenticação"
+        />
+      </div>
+      <div className="m-10 w-full md:w-1/2 lg:w-1/3 transition-all">
+        <h1 className="text-3xl font-bold mb-5">
           {modo === AuthEnum.LOGIN ? "Entre com sua conta" : "Cadastre-se na plataforma"}
         </h1>
 
@@ -43,8 +50,30 @@ export default function Auth() {
           w-full rounded-lg px-4 py-3
           bg-red-500 hover:bg-red-400 text-white `}
         >
-          Entrar com o Google
+          {modo === AuthEnum.LOGIN ? "Entrar com o Google" : "Cadastrar com o Google"}
         </button>
+
+        {modo === AuthEnum.LOGIN ? (
+          <p className="mt-8 text-center">
+            É sua primeira vez aqui?{" "}
+            <span
+              onClick={() => setModo(AuthEnum.CADASTRO)}
+              className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer"
+            >
+              Crie uma conta
+            </span>
+          </p>
+        ) : (
+          <p className="mt-8 text-center">
+            Já possui conta?{" "}
+            <span
+              onClick={() => setModo(AuthEnum.LOGIN)}
+              className="text-blue-500 hover:text-blue-700 font-semibold cursor-pointer"
+            >
+              Faça login
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );
