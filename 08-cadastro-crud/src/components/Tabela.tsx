04f8e29b99ea5357b45813c1ee540d03acebc9a5
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-01-04 14:33:31 -0300

    adicionando estilo na tabela

--- 08-cadastro-crud/src/components/Tabela.tsx
+++ 08-cadastro-crud/src/components/Tabela.tsx
@@ -8,26 +8,33 @@ export default function Tabela(props: Props) {
   function renderCabecalho() {
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="p-4">Código</th>
+        <th className="p-4">Nome</th>
+        <th className="p-4">Idade</th>
       </tr>
     );
   }
 
   function renderCorpo() {
     return props.clientes?.map((cliente, i) => (
-      <tr key={cliente.id}>
-        <td>{cliente.id}</td>
-        <td>{cliente.nome}</td>
-        <td>{cliente.idade}</td>
+      <tr key={cliente.id} className={i % 2 ? "bg-purple-200" : "bg-purple-100"}>
+        <td className="p-4">{cliente.id}</td>
+        <td className="p-4">{cliente.nome}</td>
+        <td className="p-4">{cliente.idade}</td>
       </tr>
     ));
   }
 
   return (
-    <table>
-      <thead>{renderCabecalho()}</thead>
+    <table className="w-full text-left rounded-xl overflow-hidden">
+      <thead
+        className={`
+          text-gray-100
+          bg-gradient-to-b from bg-purple-500 to-purple-800
+      `}
+      >
+        {renderCabecalho()}
+      </thead>
       <tbody>{renderCorpo()}</tbody>
     </table>
   );
