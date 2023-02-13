43d0fe5e8700bc7a1c19a77d19800631806e61d2
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-02-13 13:53:10 -0300

    corrigindo aviso de dados publicos

--- 08-cadastro-crud/src/components/Titulo.tsx
+++ 08-cadastro-crud/src/components/Titulo.tsx
@@ -7,7 +7,7 @@ export default function Titulo(props: Props) {
       <h1 className="px-5 py-2 text-2xl">{props.children}</h1>
       <div className="px-5 py-2">
         <p>⚠️ Atenção: Os dados desta página são inseridos dinamicamente pelos usuários visitantes.</p>
-        <p>O autor do projeto não assume responsabilidade pelo conteúdo aqui apresentado. ⚠️</p>
+        <p>⚠️ O autor do projeto não assume responsabilidade pelo conteúdo aqui apresentado.</p>
       </div>
       <hr className="border-2 border-purple-500" />
     </div>
